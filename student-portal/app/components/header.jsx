3e243a301ddcc6625423bb8f3993771505916b3c
Protsahan Lama <protsahanlama123@gmail.com>
2025-06-17 20:30:25 -0600

    Add files via upload
    Added header component that contains page header and a static navbar within it.

---
+++ student-portal/app/components/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <header className="text-3xl font-bold py-7 bg-gray-800 text-white w-full text-center">
+            <p>Student information</p>
+        
+        <nav className="mt-4 flex justify-center text-lg font-light">
+        <a
+          href="home" className="hover:text-indigo-400 px-4">Home</a>
+        <a href="students" className="hover:text-indigo-400 px-4">Students</a>
+        <a href="add-student" className="hover:text-indigo-400 px-4">Add Student</a>
+        <a href="contact" className="hover:text-indigo-400 px-4"> Contact</a>
+      </nav>
+    </header>
+    )
+}
+
+export default Header
