df2dfe9941e75f5019fdb03afa3cfc518a1dad20
Protsahan Lama <protsahanlama123@gmail.com>
2025-06-17 20:32:49 -0600

    Update layout.js
    Added header into the layout and fixed some indentation.

--- student-portal/app/layout.js
+++ student-portal/app/layout.js
@@ -19,10 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+          {children}
         <Footer />
       </body>
     </html>
